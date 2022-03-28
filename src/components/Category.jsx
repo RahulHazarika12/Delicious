04b0a84421f09f3d21ba1274a3594eb4256c949b
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-03-28 08:50:36 +0530

    Update Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,27 +1,31 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
-import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import { GiNoodles, GiChopsticks, GiFireBowl } from "react-icons/gi";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 function Category() {
   return (
     <List>
-      <NavLink to={"/cuisine/Italian"}>
+      <SLink to={"/cuisine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/American"}>
+      </SLink>
+      <SLink to={"/cuisine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Thai"}>
+      </SLink>
+      <SLink to={"/cuisine/Thai"}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Japanese"}>
+      </SLink>
+      <SLink to={"/cuisine/Indian"}>
+        <GiFireBowl />
+        <h4>Indian</h4>
+      </SLink>
+      <SLink to={"/cuisine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </SLink>
     </List>
   );
 }
@@ -30,5 +34,39 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: #fff;
+    font-size: 0.8rem;
+  }
+  svg {
+    color: #fff;
+    font-size: 2rem;
+  }
+  &.active {
+    background: linear-gradient(to right, #55361a 0%, #bb5e29 80%);
+
+    svg {
+      color: #fff;
+    }
+    h4 {
+      color: #fff;
+    }
+  }
 `;
 export default Category;
