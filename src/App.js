cee0e52bef0e1e5a48d4bab522c09c948bcd41b0
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-03-28 09:04:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
@@ -13,3 +15,5 @@ function App() {
 }
 
 export default App;
+
+
