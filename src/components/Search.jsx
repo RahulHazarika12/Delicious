919fa1d4080e38e1f9784ffd34c56030b6647ab6
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-03-29 08:34:57 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,36 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/" + input);
+  };
   return (
-    <FormStyle>
-      <input type="text" />
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch></FaSearch>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
+      </div>
     </FormStyle>
   );
 }
 const FormStyle = styled.form`
   margin: 0rem 20rem;
-  position: relative;
-  width: 100%;
+
+  div {
+    width: 100%;
+    position: relative;
+  }
   input {
     border: none;
     background: linear-gradient(35deg, #494949, #313131);
@@ -22,6 +40,7 @@ const FormStyle = styled.form`
     border: none;
     border-radius: 1rem;
     outline: none;
+    width: 93%;
   }
   svg {
     position: absolute;
