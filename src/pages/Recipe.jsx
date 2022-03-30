5c54999fddb02345d7bbdb572fbb71f80c397cc8
Rahul Hazarika <101569226+RahulHazarika12@users.noreply.github.com>
2022-03-30 08:57:47 +0530

    Update Recipe.jsx

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -32,4 +32,23 @@ function Recipe() {
     </DetailWrapper>
   );
 }
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: #fff;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
 
